799acda74db343f359f9c715f286abe207e3252f
DannisonArias <dannisonarias@gmail.com>
2020-04-01 21:09:59 -0400

    create password validity test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,15 +1,18 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-
-  test "username presence" do
-    user = User.new(username:"")
-    assert_not user.valid?, "username can't be blank"
+  def setup
+    @user = User.new( username: 'iamuser', password: "foobar")
   end
 
-  test "password can't be blank" do
-    user = User.new(password:"")
-    assert_not user.valid?, "password can't be blank"
+  test "username presence" do
+    @user.username = "  "
+    assert_not @user.valid?, "username can't be blank"
+  end
+
+  test "password presence" do
+    @user.password = "  "
+    assert_not @user.valid?, "password can't be blank"
   end
 
 end
