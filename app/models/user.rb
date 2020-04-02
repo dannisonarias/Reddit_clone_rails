b2add3dc91c15326fbe43d3558558b0ed2044e2f
DannisonArias <dannisonarias@gmail.com>
2020-04-02 14:24:33 -0400

    update validators

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
     has_many :comment
     has_many :posts
     validates :username,:password, presence: true
+    validates :username,uniqueness: { case_sensitive: false }
     has_secure_password
 end
